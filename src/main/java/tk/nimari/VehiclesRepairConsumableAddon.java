258dc4468bb30b98197a2c836c6c45dcd0e12012
Nimāri Murgatroyd <stefandiamond597@gmail.com>
2023-03-05 14:37:23 +0100

    Optimized everything

--- src/main/java/tk/nimari/VehiclesRepairConsumableAddon.java
+++ src/main/java/tk/nimari/VehiclesRepairConsumableAddon.java
@@ -8,14 +8,10 @@ import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -45,8 +41,8 @@ public final class VehiclesRepairConsumableAddon extends JavaPlugin {
         config.addDefault("deleteRepairItemOnRepair", false);
         config.options().copyDefaults(true);
         saveConfig();
-        Bukkit.getPluginManager().registerEvents(new Listeners(), this);
 
+        Bukkit.getPluginManager().registerEvents(new Listeners(), this);
         getLogger().log(Level.INFO, "Successful startup! Took " + (System.currentTimeMillis() - currentTime) + "ms.");
     }
 
@@ -59,59 +55,54 @@ public final class VehiclesRepairConsumableAddon extends JavaPlugin {
 
         @EventHandler
         public void interacts(EntityDamageByEntityEvent e) {
-            if (e.getDamager() instanceof Player) {
-                Player player = (Player) e.getDamager();
-                if (player.getInventory().getItemInMainHand() != null && player.getInventory().getItemInMainHand().hasItemMeta()) {
-                    if (player.getInventory().getItemInMainHand().getType() == Material.matchMaterial(config.getString("itemMaterial")) && player.getInventory().getItemInMainHand().getItemMeta().hasDisplayName() && player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().contains(config.getString("itemName"))) {
-                        e.setCancelled(true);
-                        try {
+            if (!(e.getDamager() instanceof Player)) return;
 
-                            Vehicle vehicle = VehiclesMain.getPlugin().getPlayerVehicle(player);
+            Player player = (Player) e.getDamager();
 
-                            if (vehicle.getMainStand().getHealth() != vehicle.getMainStand().getMaxHealth()) {
-                                if (config.getDouble("repairPrice") != 0.0) {
-                                    RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-                                    Economy econ = rsp.getProvider();
-                                    EconomyResponse er = econ.withdrawPlayer(player, config.getDouble("repairPrice"));
-                                    if (er.transactionSuccess()) {
+            if (player.getInventory().getItemInMainHand() == null) return;
+            if (!player.getInventory().getItemInMainHand().hasItemMeta()) return;
+            if (player.getInventory().getItemInMainHand().getType() != Material.matchMaterial(config.getString("itemMaterial")))
+                return;
+            if (!player.getInventory().getItemInMainHand().getItemMeta().hasDisplayName()) return;
+            if (!player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().contains(config.getString("itemName")))
+                return;
 
-                                        if (config.getBoolean("deleteRepairItemOnRepair"))
-                                            player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+            e.setCancelled(true);
 
-                                        if (config.getBoolean("sendMessageOnSuccessfulRepair"))
-                                            player.sendMessage(config.getString("successfulRepairMessage").replace("&", "§"));
+            try {
+                Vehicle vehicle = VehiclesMain.getPlugin().getPlayerVehicle(player);
 
-                                        vehicle.setHealth(vehicle.getMainStand().getMaxHealth());
-
-                                    } else {
-                                        if (config.getBoolean("sendMessageOnNotEnoughMoney"))
-                                            player.sendMessage(config.getString("notEnoughMoneyMessage").replace("&", "§"));
-                                    }
-
-                                } else {
-
-                                    if (config.getBoolean("deleteRepairItemOnRepair"))
-                                        player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-
-                                    if (config.getBoolean("sendMessageOnSuccessfulRepair"))
-                                        player.sendMessage(config.getString("successfulRepairMessage").replace("&", "§"));
-
-                                    vehicle.setHealth(vehicle.getMainStand().getMaxHealth());
-                                }
+                if (vehicle.getMainStand().getHealth() == vehicle.getMainStand().getMaxHealth()) {
+                    if (config.getBoolean("sendMessageOnFullHealthVehicle"))
+                        player.sendMessage(config.getString("alreadyFullHealthVehicleMessage").replace("&", "§"));
+                    return;
+                }
 
 
-                            } else {
-                                if (config.getBoolean("sendMessageOnFullHealthVehicle"))
-                                    player.sendMessage(config.getString("alreadyFullHealthVehicleMessage").replace("&", "§"));
-                            }
-                        } catch (Exception et) {
-                            if (config.getBoolean("sendMessageOnInvalidVehicle"))
-                                player.sendMessage(config.getString("invalidVehicleMessage").replace("&", "§"));
-                        }
+                if (config.getDouble("repairPrice") != 0.0) {
+                    EconomyResponse er = getServer().getServicesManager().getRegistration(Economy.class)
+                            .getProvider().withdrawPlayer(player, config.getDouble("repairPrice"));
 
+                    if (!er.transactionSuccess()) {
+                        if (config.getBoolean("sendMessageOnNotEnoughMoney"))
+                            player.sendMessage(config.getString("notEnoughMoneyMessage").replace("&", "§"));
+                        return;
                     }
                 }
+
+                if (config.getBoolean("deleteRepairItemOnRepair"))
+                    player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+
+                if (config.getBoolean("sendMessageOnSuccessfulRepair"))
+                    player.sendMessage(config.getString("successfulRepairMessage").replace("&", "§"));
+
+                vehicle.setHealth(vehicle.getMainStand().getMaxHealth());
+
+            } catch (Exception ex) {
+                if (config.getBoolean("sendMessageOnInvalidVehicle"))
+                    player.sendMessage(config.getString("invalidVehicleMessage").replace("&", "§"));
             }
+
         }
     }
 }
